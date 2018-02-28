06d54e589eeb8f0d0aafb52b50cd50011e96ee08
Raj <raj@Rajs-MacBook-Pro.local>
2018-02-28 10:06:29 +0800

    Added models and added the functionality in components

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,3 +1,4 @@
+import { Recipe } from '../recipe.model';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  abc: string = "Hello Bhai";
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is a simple recipe', 'https://cdn5.norecipes.com/wp-content/uploads/2018/02/02071116/mapo-tofu-recipe-011.jpg'),
+    new Recipe('Second Recipe', 'This is a simple recipe 2', 'http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/1506120378/MR_0917170472.jpg?')
+  ];
 
   constructor() { }
 
